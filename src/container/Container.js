e3522b7af6d3ea89ec37067798c6dca5b38c388c
Duc Vu <de.yd315@gmail.com>
2019-04-28 01:12:41 -0500

    added container component

--- src/container/Container.js
+++ src/container/Container.js
@@ -1,2 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+export default class Container extends React.Component {
+    // constructor() {
+    //     super()
+    // }
+
+    render = () => {
+        return (
+            <div className="App">
+                <header className="App-header">
+                
+                </header>
+            </div>
+        );
+    }
+    
+}
+
 
