86549157f8696e297821730df4b60f37372d5a3f
Duc Vu <de.yd315@gmail.com>
2019-05-13 09:20:51 -0500

    fixed tasks page issue

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default () => render = () =>{
-  return [
-    <h2>Log In</h2>,
-    <form>
-      {/* your form */}
-    </form>
-  ];
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Tasks
+      </header>
+    </div>
+  );
 }
 
 // export default App;
